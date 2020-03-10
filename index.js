7728829f8a4792dd8ca9b77d82a4ddc255bf9473
Zac Smith <zrs3141592@gmail.com>
2020-03-10 12:14:08 -0600

    fix hubsRouter import

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const server = require("./api/server.js");
 
 const PORT = 4000;
 server.listen(PORT, () => {
-  console.log("\n*** Server Running on http://localhost:4000 ***\n");
+  console.log(`\n*** Server Running on http://localhost:${PORT} ***\n`);
 });
